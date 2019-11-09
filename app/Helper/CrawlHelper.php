c3562cf067fe2c0a636f9fecbfe3737cbddf63ab
le quoc hieu <lequochieu284@gmail.com>
2019-11-09 11:57:14 +0700

    tool crawl vietnamnet

--- app/Helper/CrawlHelper.php
+++ app/Helper/CrawlHelper.php
@@ -1,13 +1,21 @@
 <?php
-
 namespace App\Http\Helper;
+
+require "simple_html_dom.php";
 
 // use Illuminate\Http\Request;
 
 class CrawlHelper
 {
-	public function run(){
+	public function run($link){
+		$ch = curl_init($link);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $content = curl_exec($ch);
+        curl_close($ch);
+        $dom = str_get_html($content);
 
+        return $dom;
+     
 	}
 
 }
--- app/Http/Controllers/ToolController.php
+++ app/Http/Controllers/ToolController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Website;
 use App\Category;
+use App\Http\Helper\CrawlHelper;
 
 class ToolController extends Controller
 {
@@ -22,8 +23,36 @@ class ToolController extends Controller
     	}
     }
 
-    public function crawl(Request $request){
-        dd($request->all());
+    public function crawl(Request $request, CrawlHelper $crawlHelper){
+        $dom = $crawlHelper->run("https://vietnamnet.vn/vn/the-thao/");
+
+        $content = $dom->find('.box-horizontal-style-2');
+
+
+        $highlight = $dom->find('.top-cate-new-head',0);
+
+// echo ($highlight->find(".top-cate-new-head-title p",0)); die;
+        $arr = [];
+
+        $arr[] = [
+            'title' => $highlight->find("a img",0)->alt,
+            'url' => $highlight->find("a",0)->href,
+            'image' => $highlight->find("a img",0)->src,
+            'short_content' => $highlight->find(".top-cate-new-head-title p",0)->plaintext
+        ];
+
+        // dd($arr);
+
+        
+
+        foreach ($content as $link) {
+            $arr[] = [
+                'title' => $link->find("a img",0)->alt,
+                'url' => $link->find("a",0)->href,
+                'image' => $link->find("a img",0)->src
+            ];            
+        }
+        dd($arr);
     }
 
 
